00f48cc94d6cd17836dc49a22a192126c576b550
Zich067 <ccccchinggggg@gmail.com>
2022-12-17 11:34:21 +0800

    連線測試

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -6,6 +6,7 @@
 // 3. 去讀官方文件
 const axios = require('axios');
 const fs = require("fs/promises");
+const moment = require('moment/moment');
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
 //2016,2330,2412
 
@@ -13,7 +14,8 @@ const fs = require("fs/promises");
   try {
     let stockNo = await fs.readFile('stock.txt', 'utf-8');
     // console.log(stockNo)
-    let date = '20221111';
+    let date = moment().format('YYYYMMDD');
+    // console.log(date)
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
         stockNo,
