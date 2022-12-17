2044b2ee5a7daf89257ca310cd713f5e35cdd970
Zich067 <ccccchinggggg@gmail.com>
2022-12-17 11:57:06 +0800

    env不給你看~~~

--- crawler/test.js
+++ crawler/test.js
@@ -1,16 +1,17 @@
 const mysql2 = require('mysql2/promise');
 
+require ('dotenv').config();
+
 (async()=>{
     const connection = await mysql2.createConnection({    
-    host:'127.0.0.1',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'stock_mfee31',
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PWD,
+    database:process.env.DB_DATABASE,
 });
 
-    let result = await connection.query('SELECT * FROM `stocks`');
-    let data =result[0];
+    let [data,fields] = await connection.query('SELECT * FROM `stocks`');
     // console.log(result);
     console.log(data);
     connection.end();
